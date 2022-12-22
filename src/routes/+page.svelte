5924aef39e4021a7c0858947f1f474faa5475af8
Paul Veber <paulveber56@gmail.com>
2022-12-22 23:03:43 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+    <title>Activity</title>
+    <meta name="description" content="Plan activities with your friends" />
+</svelte:head>
+<h1>Activity</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
